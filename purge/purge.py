561e5f1981f9865bed96ba7a97c8cb927ded0678
robwayne <robwaynegordon@gmail.com>
2017-12-24 12:09:33 -0500

    Added a positional argument to the script

--- purge/purge.py
+++ purge/purge.py
@@ -1,17 +1,27 @@
 #!/usr/bin/env python3
+
 import os
 import shutil
+import argparse
 
 
 def main():
-    src = "/Users/robudhabi"
+    args = getArgs()
+    src = args.root
     directories = os.listdir(src)
+    print(directories)
     directories = getRelevantDirectories(directories)
 
 
+def getArgs():
+    parse = argparse.ArgumentParser()
+    parse.add_argument("root",help="the root directory to purge children tree")
+    args = parse.parse_args()
+    return args
+
 def getRelevantDirectories(directories):
     relevantDirectories = ["Desktop", "Document", "Downloads"]
-    for dir in directories:
-        if dir[0]
+
+
 if __name__ == "__main__":
     main()
